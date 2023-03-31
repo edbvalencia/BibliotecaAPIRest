62e77b373fd0416c6cca178458d937638d46a86b
ebvalencia <ebvalenciap@gmail.com>
2023-03-31 04:58:03 -0500

    inicio del proyecto

---
+++ src/main/java/com/edbvalencia/BibliotecaAPIRest/BibliotecaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.edbvalencia.BibliotecaAPIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edbvalencia/BibliotecaAPIRest/BibliotecaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edbvalencia.BibliotecaAPIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
